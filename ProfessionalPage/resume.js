2ba90af6cd4057d33969b2f1a10ad02796c26ca3
tlindsay8 <giddeon2012@gmail.com>
2018-08-14 14:19:53 -0400

    first commit/pages do not work

---
+++ ProfessionalPage/resume.js
@@ -0,0 +1,9 @@
+function showText(div){
+  var x = document.getElementById(div);
+  if(x.style.display == "none"){
+    x.style.display = "block";
+  }
+  else {
+    x.style.display = "none";
+  }
+}
